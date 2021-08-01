bd9967cf4701d54c36370b34273085bdee6923c3
samuel <samuelsoutosilva14@gmail.com>
2021-07-31 22:00:40 -0300

    adiciona exercicios 9 e 10

---
+++ Mod01-Fundamentos/Bloco4-Introdução-à-JavaScript-e-Lógica-de-Programação/Dia01-Bloco4/PrimeirosPassos/Ex09-D01-B04.js
@@ -0,0 +1,11 @@
+//9 - Escreva um programa que defina três números em constantes e retorne true se pelo menos uma das três for ímpar. Caso contrário, ele retorna false .
+
+let N1= 9;
+let N1= 13;
+let N3 = 2;
+
+if (N1% 2 == 0 || N2 % 2 == 0 || N3 % 2 == 0 ) {
+  console.log("Número ímpar: " + true)
+} else {
+  console.log("Número é ímpar: " + false);
+}
