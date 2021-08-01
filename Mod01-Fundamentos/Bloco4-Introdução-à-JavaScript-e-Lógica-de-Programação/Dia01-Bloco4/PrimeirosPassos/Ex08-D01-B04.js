b840fceffd3e4354e39f6c47b64480dcabfb2733
samuel <samuelsoutosilva14@gmail.com>
2021-07-31 22:00:04 -0300

    adiciona exercicios 7 e 8

---
+++ Mod01-Fundamentos/Bloco4-Introdução-à-JavaScript-e-Lógica-de-Programação/Dia01-Bloco4/PrimeirosPassos/Ex08-D01-B04.js
@@ -0,0 +1,12 @@
+//8 - Escreva um programa que defina três números em constantes e retorne true se pelo menos uma das três for par. Caso contrário, ele retorna false .
+
+let N1 = 9;
+let N2 = 13;
+let N3 = 5;
+
+if (N1 % 2 == 0 || N2 % 2 == 0 || N3 % 2 == 0 ) {
+  console.log("Número par: " + true)
+} else {
+  console.log("Número é par: " + false);
+}
+
