88f4a3ee0a575c80229fe2e2f1aaaac959bca080
samuel <samuelsoutosilva14@gmail.com>
2021-08-27 20:08:53 -0300

    Adiciona ex02 - parte 1

--- Mod01-Fundamentos/Bloco7-JavaScriptES6-testesUnitários/Dia01-Bloco7/parte-1/index.js
+++ Mod01-Fundamentos/Bloco7-JavaScriptES6-testesUnitários/Dia01-Bloco7/parte-1/index.js
@@ -1,5 +1,7 @@
-const testingScope = escopo => { 
-  if (escopo === true) { 
+// ex01 - parte 1
+
+const testingScope = escopo => {
+  if (escopo === true) {
     let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
     ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
     console.log(ifScope);
@@ -10,3 +12,10 @@ const testingScope = escopo => {
 }
 
 testingScope(true);
+
+// ex02 - parte 1
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+const ordenado = oddsAndEvens.sort((a, b) => a - b);
+console.log(`Os números ${ordenado} se encontram ordenados de forma crescente !`);
+
