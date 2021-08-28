3ecf47d9c1180a9e50357bb41b79d96b4732975f
samuel <samuelsoutosilva14@gmail.com>
2021-08-28 12:06:23 -0300

    Adiciona ex02 - parte 2

--- Mod01-Fundamentos/Bloco7-JavaScriptES6-testesUnitários/Dia01-Bloco7/parte-2/index.js
+++ Mod01-Fundamentos/Bloco7-JavaScriptES6-testesUnitários/Dia01-Bloco7/parte-2/index.js
@@ -3,3 +3,21 @@
 const factorial = numero => numero > 1 ? numero * factorial(numero - 1) : 1;
 console.log(factorial(5));
 
+//ex02 - part 2
+
+const longestWord = frase => {
+    let palavrasArray = frase.split(' ');
+    let maiorCompri = 0;
+    let result = '';
+
+    for (const palavra of palavrasArray) {
+        if (palavra.length > maiorCompri) {
+            maiorCompri = palavra.length;
+            result = palavra;
+        }
+    }
+
+    return result;
+}
+
+console.log(longestWord("Antonio foi no banheiro e não sabemos o que aconteceu"));
