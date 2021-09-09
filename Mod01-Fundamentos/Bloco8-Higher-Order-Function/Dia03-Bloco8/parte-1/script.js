0e38a2eb0a0078ec08d6a05dbc708b5b96906c9e
samuel <samuelsoutosilva14@gmail.com>
2021-09-09 13:52:11 -0300

    Adiciona Exercicio 3 (8.3)

--- Mod01-Fundamentos/Bloco8-Higher-Order-Function/Dia03-Bloco8/parte-1/script.js
+++ Mod01-Fundamentos/Bloco8-Higher-Order-Function/Dia03-Bloco8/parte-1/script.js
@@ -81,34 +81,6 @@ const expectedResult = [
   assert.deepStrictEqual(formatedBookNames(), expectedResult);
 
 //Exercicio 2 
-
-const expectedResult = [
-    {
-      age: 31,
-      author: 'Isaac Asimov',
-    },
-    {
-      age: 38,
-      author: 'H. P. Lovecraft',
-    },
-    {
-      age: 39,
-      author: 'Stephen King',
-    },
-    {
-      age: 43,
-      author: 'George R. R. Martin',
-    },
-    {
-      age: 45,
-      author: 'Frank Herbert',
-    },
-    {
-      age: 62,
-      author: 'J. R. R. Tolkien',
-    },
-  ];
-  
   function nameAndAge() {
     return books
     .map((book) => (
@@ -121,3 +93,13 @@ const expectedResult = [
   }
   
   assert.deepStrictEqual(nameAndAge(), expectedResult);
+
+// Exercicio 3 
+
+ 
+function fantasyOrScienceFiction() {
+  const genero = books.filter((book) => book.genre === "Fantasia" || book.genre === "Ficção Científica")
+  return genero;
+}
+
+assert.deepStrictEqual(fantasyOrScienceFiction(), expectedResult);
