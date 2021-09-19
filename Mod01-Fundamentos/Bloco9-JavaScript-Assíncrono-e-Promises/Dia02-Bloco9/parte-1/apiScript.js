5d54dc0b6d81c613b9799be745b417b24eb2e4c8
samuel <samuelsoutosilva14@gmail.com>
2021-09-18 21:25:29 -0300

    adiciona exercicios 9.2

---
+++ Mod01-Fundamentos/Bloco9-JavaScript-Assíncrono-e-Promises/Dia02-Bloco9/parte-1/apiScript.js
@@ -0,0 +1,136 @@
+// Exercicio 1    
+const API_URL = 'https://icanhazdadjoke.com/';
+
+const fetchJoke = () => {
+  const myObject = {
+    method: 'GET',
+    headers: { 'Accept': 'application/json' }
+  };
+
+  fetch(API_URL, myObject)
+    .then(response => response.json())
+    .then(data => {
+      const h2 = document.getElementById('jokeContainer');
+      const text = data.joke;
+      h2.innerText = text;
+    });
+};
+
+window.onload = () => fetchJoke();
+
+// Exercicio 2 - Usado o gabarito nesse exercicio
+
+const fetchPromise = () => {
+  const myPromise = new Promise((resolve, reject) => {
+    const myArray = Array.from(
+      { length: 10 },
+      () =>  Math.floor(Math.random() * 50) + 1
+    );
+    const sum = myArray.map(number => number * number)
+                       .reduce((sum, number) => sum + number);
+
+    (sum < 8000) ? resolve() : reject();
+  });
+
+  myPromise
+    .then(() => console.log('Promise resolvida'))
+    .catch(() => console.log('Promise rejeitada'));
+};
+
+fetchPromise();
+
+// Exercicio 3
+const fetchPromise = () => {
+  const myPromise = new Promise((resolve, reject) => {
+    const myArray = Array.from(
+      { length: 10 },
+      () =>  Math.floor(Math.random() * 50) + 1
+    );
+    const sum = myArray.map(number => number * number)
+                       .reduce((number, acc) => number + acc, 0);
+
+    (sum < 8000) ? resolve(sum) : reject();
+  });
+
+  myPromise
+    .then(sum => [2, 3, 5, 10].map(number => sum / number))
+    .catch(() => console.log('Promise rejeitada'));
+};
+
+fetchPromise();
+
+// Exercicio 4
+const fetchPromise = () => {
+  const myPromise = new Promise((resolve, reject) => {
+    const myArray = Array.from(
+      { length: 10 },
+      () =>  Math.floor(Math.random() * 50) + 1
+    );
+    const sum = myArray.map(number => number * number)
+                       .reduce((number, acc) => number + acc, 0);
+
+    (sum < 8000) ? resolve(sum) : reject();
+  });
+
+  myPromise
+    .then(sum => [2, 3, 5, 10].map(number => sum / number))
+    .catch(() =>
+      console.log('É mais de oito mil! Essa promise deve estar quebrada!')
+    );
+};
+
+fetchPromise();
+
+//  Exercicio 5
+const fetchPromise = () => {
+  const myPromise = new Promise((resolve, reject) => {
+    const myArray = Array.from(
+      { length: 10 },
+      () =>  Math.floor(Math.random() * 50) + 1
+    );
+    const sum = myArray.map(number => number * number)
+                       .reduce((number, acc) => number + acc, 0);
+
+    (sum < 8000) ? resolve(sum) : reject();
+  });
+
+  myPromise
+    .then(sum => {
+      const arr = [2, 3, 5, 10].map(number => sum / number)
+      return arr;
+    }).then(arr => arr.reduce((acc, interado) => interado + acc), 0)
+    .catch(() => console.log('É mais de oito mil! Essa promise deve estar quebrada!'));
+};
+
+fetchPromise();
+
+// Exercicio 6
+
+const sumRandomNumbers = () => {
+  const myArray = Array.from(
+    { length: 10 },
+    () => Math.floor(Math.random() * 50) + 1
+  );
+  const sum = myArray.map(number => number * number)
+    .reduce((number, acc) => number + acc, 0);
+
+  if (sum >= 8000) {
+    throw new Error();
+  }
+
+  return sum;
+}
+
+const sumArrayFromSum = (sum) => [2, 3, 5, 10].map(number => sum / number)
+  .reduce((number, acc) => number + acc);
+
+const fetchPromise = async () => {
+  try {
+    const sum = await sumRandomNumbers();
+    const sumFromSum = await sumArrayFromSum(sum);
+  } catch (error) {
+    console.log('É mais de oito mil! Essa promise deve estar quebrada!');
+  }
+}
+
+fetchPromise();
