eeeb8d48f5739f19d2c04fca7f14766dfad666ed
samuel <samuelsoutosilva14@gmail.com>
2021-09-23 13:00:28 -0300

    adiciona exercicio 01 (10.1)

---
+++ Mod01-Fundamentos/Bloco10-TestesAutomatizadosComJest/Dia01-Bloco10/parte-1/script.test.js
@@ -0,0 +1,19 @@
+const sum = require("./script.js")
+
+describe("verifica a soma de dois números inteiros", () => {
+    it("verifica se a soma de 4 + 5 é 9",() => {
+      expect(sum(4,5)).toBe(9)
+    })
+
+    it("Teste se o retorno de sum(0, 0) é 0", () => {
+      expect(sum(0,0)).toBe(0)
+    })
+
+    it("Teste se a função sum lança um erro quando os parâmetros são 4 e '5' (string 5)", () => {
+        expect(() => { sum(4, '5') }).toThrow();
+    })
+    
+    it("Teste se a função sum lança um erro quando os parâmetros são 4 e '5' (string 5)",() => {
+      expect(() => { sum(3, '5')}).toThrow("parameters must be numbers");
+    })
+})
