3dcf7f03bde37c36cf0a0e17cebfb6e02119914b
samuel <samuelsoutosilva14@gmail.com>
2021-10-11 18:15:07 -0300

    adiciona arquivos

---
+++ Mod02-FrontEnd/Bloco11-Componentes-Estados-Eventos-Form-React/Dia01-Bloco11/(conteudoFixarB04D11)/fancy-buttons/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './App.css';
+
+
+function handleClick() {
+  console.log('Clicou no botão 1')
+}
+
+function handleClick2() {
+  console.log('Clicou no botão! 2')
+}
+
+function handleClick3() {
+  console.log('Clicou no botão! 3')
+}
+class App extends React.Component {
+  
+  render() {
+    return (
+      <div className="App">
+         <button onClick={handleClick}>Meu botão</button>
+         <button onClick={handleClick2}>Meu botão 2</button>
+         <button onClick={handleClick3}>Meu botão 3</button>
+      </div>
+    );
+  }
+}
+
+export default App;
