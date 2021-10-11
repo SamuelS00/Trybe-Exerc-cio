bb59e812037c427665ea1a1d9bb9cff79fd4e506
samuel <samuelsoutosilva14@gmail.com>
2021-10-11 19:13:26 -0300

    adiciona exercicios 5, 6 e 7 do bloco 11 dia 1

--- Mod02-FrontEnd/Bloco11-Componentes-Estados-Eventos-Form-React/Dia01-Bloco11/(conteudoFixarB04D11)/fancy-buttons/src/App.js
+++ Mod02-FrontEnd/Bloco11-Componentes-Estados-Eventos-Form-React/Dia01-Bloco11/(conteudoFixarB04D11)/fancy-buttons/src/App.js
@@ -6,6 +6,11 @@ import './App.css';
 class App extends React.Component {
   constructor() {
     super() 
+    this.state = {
+      clicks: 0,
+      clicks2: 0,
+      clicks3: 0,
+    }
 
     this.handleClick = this.handleClick.bind(this)
     this.handleClick2 = this.handleClick2.bind(this)
@@ -13,23 +18,29 @@ class App extends React.Component {
   }
  
   handleClick() {
-    console.log('Clicou no botão! 1');
+    this.setState((estadoAnterior, _props) => ({
+      clicks: estadoAnterior.clicks + 1
+    }))
   }
 
   handleClick2() {
-    console.log('Clicou no botão! 2');
+    this.setState((estadoAnterior, _props) => ({
+      clicks2: estadoAnterior.clicks2 + 1
+    }))
   }
 
   handleClick3() {
-    console.log('Clicou no botão! 3');
+    this.setState((estadoAnterior, _props) => ({
+      clicks3: estadoAnterior.clicks3 + 1
+    }))
   }
 
   render() {
     return (
       <div className="App">
-         <button onClick={this.handleClick}>Meu botão</button>
-         <button onClick={this.handleClick2}>Meu botão 2</button>
-         <button onClick={this.handleClick3}>Meu botão 3</button>
+         <button onClick={this.handleClick}>{this.state.clicks}</button>
+         <button onClick={this.handleClick2}>{this.state.clicks2}</button>
+         <button onClick={this.handleClick3}>{this.state.clicks3}</button>
       </div>
     );
   }
