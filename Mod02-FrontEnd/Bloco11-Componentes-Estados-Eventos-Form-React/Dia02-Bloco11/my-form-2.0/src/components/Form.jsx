7c6c0df9b4c0e23c143802ed55a0b37c6a2f47bb
Samuel <samuelsoutosilva14@gmail.com>
2021-12-21 10:41:13 -0300

    Adiciona Type e Select

--- Mod02-FrontEnd/Bloco11-Componentes-Estados-Eventos-Form-React/Dia02-Bloco11/my-form-2.0/src/components/Form.jsx
+++ Mod02-FrontEnd/Bloco11-Componentes-Estados-Eventos-Form-React/Dia02-Bloco11/my-form-2.0/src/components/Form.jsx
@@ -1,41 +1,15 @@
 import React from "react";
-import Option from './Option'
 import Name from './Name'
 import Email from './Email'
 import Cpf from "./Cpf";
 import Endereco from "./Endereco";
+import Cidade from "./Cidade";
+import Select from "./Select"
+import states from "../data";
+import Type from "./Type";
+
 class Form extends React.Component {
   render() {
-    const states = [
-        ['AC', 'Acre'],
-        ['AL', 'Alagoas'],
-        ['AP', 'Amapá'],
-        ['AM', 'Amazonas'],
-        ['BA', 'Bahia'],
-        ['CE', 'Ceará'],
-        ['DF', 'Distrito Federal'],
-        ['ES', 'Espírito Santo'],
-        ['GO', 'Goías'],
-        ['MA', 'Maranhão'],
-        ['MT', 'Mato Grosso'],
-        ['MS', 'Mato Grosso do Sul'],
-        ['MG', 'Minas Gerais'],
-        ['PA', 'Pará'],
-        ['PB', 'Paraíba'],
-        ['PR', 'Paraná'],
-        ['PE', 'Pernambuco'],
-        ['PI', 'Piauí'],
-        ['RJ', 'Rio de Janeiro'],
-        ['RN', 'Rio Grande do Norte'],
-        ['RS', 'Rio Grande do Sul'],
-        ['RO', 'Rondônia'],
-        ['RR', 'Roraíma'],
-        ['SC', 'Santa Catarina'],
-        ['SP', 'São Paulo'],
-        ['SE', 'Sergipe'],
-        ['TO', 'Tocantins'],
-    ]
-
     return (
       <form>
         <fieldset>
@@ -43,27 +17,9 @@ class Form extends React.Component {
           <Email />
           <Cpf />
           <Endereco />
-          <input type="text" placeholder="Cidade" />
-          <select name="select">
-            {states.map((state => <Option state={ state } />))}
-          </select>
-          <div >
-            <label htmlFor="residenciaCasa">Casa: </label>
-            <input
-              type="radio"
-              name="residencia"
-              value="casa"
-              id="residenciaCasa"
-            />
-
-            <label htmlFor="residenciaApartamento">Apartamento: </label>
-            <input
-              type="radio"
-              name="residencia"
-              value="Apartamento"
-              id="residenciaApartamento"
-            />
-          </div>
+          <Cidade />
+          <Select states={ states } />
+          <Type />
         </fieldset>
       </form>
     );
