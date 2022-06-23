9e986a51134e75f4e47c85c0a3d5335c8d0b942e
Samuel <samuelsoutosilva14@gmail.com>
2022-06-23 17:10:11 -0300

    chore: adicona dependencias do dotenv

--- Mod03-BackEnd/Bloco23-ArquiteturaRest-e-Restful/Dia01-Bloco23/exercicios-fixar/model-exemplo/src/helpers/connection.js
+++ Mod03-BackEnd/Bloco23-ArquiteturaRest-e-Restful/Dia01-Bloco23/exercicios-fixar/model-exemplo/src/helpers/connection.js
@@ -1,9 +1,10 @@
 const mysql = require('mysql2/promise');
+require('dotenv').config();
 
 const connection = mysql.createPool({
-	host: 'localhost',
-	user: 'trybe',
-	password: 'trybe12345',
-	database: 'model_example' });
+	host: process.env.HOST,
+	user: process.env.USER,
+	password: process.env.PASSWORD,
+	database: process.env.DATABASE });
 
 module.exports = connection;
