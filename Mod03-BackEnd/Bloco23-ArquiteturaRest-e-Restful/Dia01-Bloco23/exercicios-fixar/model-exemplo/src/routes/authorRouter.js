c0f53bad7945cf79766c3dbb6a7892d83fd9ecf1
Samuel <samuelsoutosilva14@gmail.com>
2022-06-23 15:32:23 -0300

    feat: adiciona model findById em author

--- Mod03-BackEnd/Bloco23-ArquiteturaRest-e-Restful/Dia01-Bloco23/exercicios-fixar/model-exemplo/src/routes/authorRouter.js
+++ Mod03-BackEnd/Bloco23-ArquiteturaRest-e-Restful/Dia01-Bloco23/exercicios-fixar/model-exemplo/src/routes/authorRouter.js
@@ -9,4 +9,14 @@ router.get('/', async(_req, res) => {
    res.status(200).json(authors);
 });
 
+router.get('/:id', async (req, res) => {
+	const { id } = req.params;
+
+	const author = await Author.findById(id);
+
+	if (!author) return res.status(404).json({ message: 'Not found' });
+
+	res.status(200).json(author);
+});
+
 module.exports = router;
