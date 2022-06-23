48784d7dca48cb3c13e71e7fc2a5378bdcf7b90f
Samuel <samuelsoutosilva14@gmail.com>
2022-06-23 15:23:41 -0300

    feat: adiciona rota books/search GET

--- Mod03-BackEnd/Bloco23-ArquiteturaRest-e-Restful/Dia01-Bloco23/exercicios-fixar/model-exemplo/src/routes/bookRouter.js
+++ Mod03-BackEnd/Bloco23-ArquiteturaRest-e-Restful/Dia01-Bloco23/exercicios-fixar/model-exemplo/src/routes/bookRouter.js
@@ -3,7 +3,19 @@ const router = express.Router();
 
 const Books = require('../models/Book');
 
-router.get('/', async(_req, res) => {
+router.get('/search', async(req, res) => {
+    const { name } = req.query;
+
+    const books = await Books.getAll();
+
+    const booksFiltered = (
+        books.filter((b) => b.author.includes(name) || b.title.includes(name))
+    );
+
+    res.status(200).json(booksFiltered);
+});
+
+router.get('/', async(req, res) => {
     const books = await Books.getAll();
 
     res.status(200).json(books);
