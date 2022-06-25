365d607344535ce39151269cce2dc160eb85b0da
Samuel <samuelsoutosilva14@gmail.com>
2022-06-24 21:50:45 -0300

    feat: adiciona rota PUT editar user

--- Mod03-BackEnd/Bloco23-ArquiteturaRest-e-Restful/Dia01-Bloco23/exercicios/CRUDuser/src/routes/userRouter.js
+++ Mod03-BackEnd/Bloco23-ArquiteturaRest-e-Restful/Dia01-Bloco23/exercicios/CRUDuser/src/routes/userRouter.js
@@ -22,6 +22,35 @@ router.get('/:id', async (req, res) => {
   res.status(200).json(user)
 });
 
+
+router.put('/:id', async (req, res) => {
+  const { id } = req.params;
+  const { first_name, last_name, email, password } = req.body;
+
+  const user = await UserModel.findById(id);
+
+  if(!user) {
+    return res.status(404).json({ error: true, message: 'Usuário não encontrado'});
+  }
+
+  const isError = UserValidate.isNewUserValid(first_name, last_name, email, password);
+
+  if(isError.error) {
+    return res.status(400).json({ error: true, message: isError.message});
+  } 
+
+  await UserModel.update(id, first_name, last_name, email, password);
+
+  const newUser = {
+    id, 
+    first_name,
+    last_name,
+    email,
+    password }
+
+  res.status(200).json(newUser);
+});
+
 router.post('/', async (req, res) => {
   const { first_name, last_name, email, password } = req.body;
 
