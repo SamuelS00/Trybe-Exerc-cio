cc0e28a2c143e0a63f3c90dd4da1b3d1623d669d
Samuel <samuelsoutosilva14@gmail.com>
2022-06-25 16:18:51 -0300

    feat: adiciona errorHandler no index

--- Mod03-BackEnd/Bloco23-ArquiteturaRest-e-Restful/Dia02-Bloco23/CEPlookUp/src/index.js
+++ Mod03-BackEnd/Bloco23-ArquiteturaRest-e-Restful/Dia02-Bloco23/CEPlookUp/src/index.js
@@ -4,12 +4,15 @@ require('dotenv').config();
 const PORT = process.env.PORT;
 
 const app = express();
+
 const bodyParser = require('body-parser');
+app.use(bodyParser.json());
 
 const cepRouter = require('./routes/cepRouter');
+const ErrorHandler = require('./middleware/ErrorHandler');
 app.use('/cep', cepRouter);
 
-app.use(bodyParser.json());
+app.use(ErrorHandler);
 
 app.listen(PORT, () => {
     console.log(`rodando na porta ${PORT}`)
