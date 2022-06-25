97f4c44a95ae14dbec6630d9fb0063a9184c9745
Samuel <samuelsoutosilva14@gmail.com>
2022-06-25 16:19:52 -0300

    feat: adiciona middleware errorHandler

---
+++ Mod03-BackEnd/Bloco23-ArquiteturaRest-e-Restful/Dia02-Bloco23/CEPlookUp/src/middleware/ErrorHandler.js
@@ -0,0 +1,18 @@
+const httpsStatusCode = require('../helpers/httpsStatusCode');
+
+const ErrorHandler = (err, _req, res, _next) => {
+    if(err.statusCode) {
+        return res.status(err.statusCode).json({ message: err.message });
+    };
+
+    console.log(err);
+
+    res.status(httpsStatusCode.INTERNAL_SERVER).json({ message: 'Erro interno do servidor'});
+};
+
+module.exports = ErrorHandler;
+
+
+// vai ser usado para centralizar;
+
+// colocar a verificação de erros vindos do joi;
