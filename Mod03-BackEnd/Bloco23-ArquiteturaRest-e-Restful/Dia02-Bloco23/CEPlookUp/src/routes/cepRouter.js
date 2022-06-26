04c2b1d7b3576d6b216820480a9068a5be1651f9
Samuel <samuelsoutosilva14@gmail.com>
2022-06-25 22:02:31 -0300

    feat: adiciona controller no router

--- Mod03-BackEnd/Bloco23-ArquiteturaRest-e-Restful/Dia02-Bloco23/CEPlookUp/src/routes/cepRouter.js
+++ Mod03-BackEnd/Bloco23-ArquiteturaRest-e-Restful/Dia02-Bloco23/CEPlookUp/src/routes/cepRouter.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+const rescue = require('express-rescue');
+
+const CepController = require('../controllers/cepController');
+
 router.get('/ping', (req, res) => {
     res.status(200).json({ message: 'pong!'});    
-})
+});
+
+router.get('/:cep', rescue(CepController.findCepAddress));
 
 module.exports = router;
