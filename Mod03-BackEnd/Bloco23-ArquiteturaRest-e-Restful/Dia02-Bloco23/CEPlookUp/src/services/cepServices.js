0a119eddfa14c3a2ed0c56086ee4935e02aba307
Samuel <samuelsoutosilva14@gmail.com>
2022-06-26 20:02:17 -0300

    feat: adiciona services  cep address

--- Mod03-BackEnd/Bloco23-ArquiteturaRest-e-Restful/Dia02-Bloco23/CEPlookUp/src/services/cepServices.js
+++ Mod03-BackEnd/Bloco23-ArquiteturaRest-e-Restful/Dia02-Bloco23/CEPlookUp/src/services/cepServices.js
@@ -1,8 +1,11 @@
 const CepModel = require('../models/cepModels');
 
 const validateCep = require('../helpers/validateCep');
-const notFound = require('../error/ErrorNotFound')
-const invalidData = require('../error/ErrorInvalidData')
+const validateCepAddress = require('../helpers/validateNewCepAddress');
+
+const notFound = require('../error/ErrorNotFound');
+const invalidData = require('../error/ErrorInvalidData');
+const conflict = require('../error/ErrorConflict');
 
 const findCepAddress = async (cep) => {
     if(!validateCep(cep)) throw invalidData('CEP inválido');
@@ -14,6 +17,20 @@ const findCepAddress = async (cep) => {
     return result;
 };
 
+const createCepAddress = async (cep, logradouro, bairro, localidade, uf) => {
+    
+    validateCepAddress(cep, logradouro, bairro, localidade, uf);
+
+    const result = await CepModel.findCepAddress(cep, logradouro, bairro, localidade, uf);
+
+    if(result.length > 0) throw conflict('CPF já existente');
+
+    const address = await CepModel.createCepAddress(cep, logradouro, bairro, localidade, uf);
+
+    return address;
+};
+
 module.exports = {
     findCepAddress,
+    createCepAddress,
 }
