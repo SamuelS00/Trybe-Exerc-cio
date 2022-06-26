ceed131598213d11179492205e8a6669d721937f
Samuel <samuelsoutosilva14@gmail.com>
2022-06-26 20:03:08 -0300

    feat: adiciona model create add address

--- Mod03-BackEnd/Bloco23-ArquiteturaRest-e-Restful/Dia02-Bloco23/CEPlookUp/src/models/cepModels.js
+++ Mod03-BackEnd/Bloco23-ArquiteturaRest-e-Restful/Dia02-Bloco23/CEPlookUp/src/models/cepModels.js
@@ -8,8 +8,20 @@ const findCepAddress = async (cep) => {
     const [result] = await connection.execute(query, [treatedCep]);
 
     return result;
-}
+};
+
+const createCepAddress = async (cep, logadouro, bairro, localidade, uf) => {
+    const query = (
+       `INSERT INTO ceps (cep, logradouro, bairro, localidade, uf) 
+        VALUES (?, ?, ?, ?, ?);`
+    );
+
+    await connection.execute(query, [ cep, logadouro, bairro, localidade, uf]);
+    
+    return { cep, logadouro, bairro, localidade, uf };
+};
 
 module.exports = { 
-    findCepAddress
+    findCepAddress,
+    createCepAddress,
 }
