fc5c556a212d89a0331b7c0805710fd2b999f3e9
Samuel <samuelsoutosilva14@gmail.com>
2022-06-26 20:01:59 -0300

    feat: adiciona controller cep address

--- Mod03-BackEnd/Bloco23-ArquiteturaRest-e-Restful/Dia02-Bloco23/CEPlookUp/src/controllers/cepController.js
+++ Mod03-BackEnd/Bloco23-ArquiteturaRest-e-Restful/Dia02-Bloco23/CEPlookUp/src/controllers/cepController.js
@@ -1,32 +1,37 @@
-// chamar o service
+const httpsStatusCode = require("../helpers/httpsStatusCode");
+const CepService = require("../services/cepServices");
 
-// colocar o try cat aqui.
-
-// aqui coloco as validações que utilização req e res. ( Não são regras de negocio. );
-
-// mensagens personalizadas e status;
-
-// chamar o httpsStatusCode. Mas fácil para encontrar o tipo de erro.
-
-// centralizar os erros da aplicação no middledware de erro.
-
-
-const httpsStatusCode = require('../helpers/httpsStatusCode');
-const CepService = require('../services/cepServices');
-
-const findCepAddress =  async (req, res, next) => {
+const findCepAddress = async (req, res, next) => {
     const { cep } = req.params;
 
     try {
-       const result = await CepService.findCepAddress(cep);
+        const result = await CepService.findCepAddress(cep);
 
-       res.status(httpsStatusCode.OK).json(result);
-    } catch(err) {
-       next(err);
+        res.status(httpsStatusCode.OK).json(result);
+    } catch (err) {
+        next(err);
+    }
+};
+
+const createCepAddress = async (req, res, next) => {
+    const { cep, logradouro, bairro, localidade, uf } = req.body;
+
+    try {
+        const result = await CepService.createCepAddress(
+            cep,
+            logradouro,
+            bairro,
+            localidade,
+            uf
+        );
+
+        res.status(httpsStatusCode.OK).json(result);
+    } catch (err) {
+        next(err);
     }
 };
 
 module.exports = {
     findCepAddress,
+    createCepAddress,
 };
-
