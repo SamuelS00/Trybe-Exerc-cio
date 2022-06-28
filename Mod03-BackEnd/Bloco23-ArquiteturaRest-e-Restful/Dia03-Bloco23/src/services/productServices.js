87a9bb5ca7f2841275af52c101191d40815a7052
Samuel <samuelsoutosilva14@gmail.com>
2022-06-28 19:40:44 -0300

    fix: return of the update method in the services

--- Mod03-BackEnd/Bloco23-ArquiteturaRest-e-Restful/Dia03-Bloco23/src/services/productServices.js
+++ Mod03-BackEnd/Bloco23-ArquiteturaRest-e-Restful/Dia03-Bloco23/src/services/productServices.js
@@ -2,8 +2,14 @@ const ProductModel = require('../models/productModel');
 const validateBody = require('../helpers/validateBody');
 const validateId = require('../helpers/validateId');
 
+const invalidData = require('../errors/invalidData');
+
+const INVALID_PRODUCT_ID = (id) => `there is no product with id ${id}`
+
 const add = async (name, brand) => {
-  validateBody(name, brand);
+  const { error } = validateBody(name, brand);
+
+  if(error) throw invalidData(error.message);
 
   const newProduct = await ProductModel.add(name, brand);
   return newProduct;
@@ -15,21 +21,29 @@ const getAll = async () => {
 };
 
 const getById = async (id) => {
-  await validateId(id);
+  const isValidId = await validateId(id);
 
+  if(isValidId) throw invalidData(INVALID_PRODUCT_ID(id));
+  
   const product = await ProductModel.getById(id);
   return product;
 };
 
 const update = async (id, name, brand) => {
-  await validateId(id);
-  validateBody(name, brand)
+  const isValidId = await validateId(id);
+  const { error } = validateBody(name, brand)
+  
+  if(isValidId) throw invalidData(INVALID_PRODUCT_ID(id));
+  if(error) throw invalidData(error.message);
 
-  await ProductModel.update(id, name, brand);
+  const updatedProduct = await ProductModel.update(id, name, brand);
+  return updatedProduct;
 };
 
 const exclude = async (id) => {
-  await validateId(id);
+  const isValidId = await validateId(id);
+
+  if(isValidId) throw invalidData(INVALID_PRODUCT_ID(id));
 
   await ProductModel.exclude(id);
 };
