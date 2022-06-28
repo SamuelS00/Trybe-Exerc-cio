c72a49fef7c224c355115196af9c9da0ca80e2f1
Samuel <samuelsoutosilva14@gmail.com>
2022-06-28 19:36:23 -0300

    fix: return from validation by id

--- Mod03-BackEnd/Bloco23-ArquiteturaRest-e-Restful/Dia03-Bloco23/src/helpers/validateId.js
+++ Mod03-BackEnd/Bloco23-ArquiteturaRest-e-Restful/Dia03-Bloco23/src/helpers/validateId.js
@@ -1,10 +1,10 @@
-const invalidData = require('../errors/invalidData');
 const ProductModel = require('../models/productModel');
 
 const validateId = async (id) => {
   const product = await ProductModel.getById(id);
+  if(product === null) return true;
 
-  if(product === null) throw invalidData(`there is no product with id ${id}`);
+  return false;
 };
 
 module.exports = validateId;
