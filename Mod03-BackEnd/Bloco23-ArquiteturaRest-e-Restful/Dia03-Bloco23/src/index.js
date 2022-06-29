7a4a53ec8112dd789692a1e05135dc5ce372397f
Samuel <samuelsoutosilva14@gmail.com>
2022-06-29 16:46:27 -0300

    refactor: remove rescue  from index

--- Mod03-BackEnd/Bloco23-ArquiteturaRest-e-Restful/Dia03-Bloco23/src/index.js
+++ Mod03-BackEnd/Bloco23-ArquiteturaRest-e-Restful/Dia03-Bloco23/src/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const productsRouter = require('./routes/productsRouter');
 const ErrorHandler = require('./middleware/ErrorHandler');
-const rescue = require('express-rescue');
 // Doc 
 const swaggerUi = require('swagger-ui-express');
 const swaggerFile = require('../swagger_output.json')
@@ -11,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/products', rescue(productsRouter));
+app.use('/products', productsRouter);
 app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(ErrorHandler);
