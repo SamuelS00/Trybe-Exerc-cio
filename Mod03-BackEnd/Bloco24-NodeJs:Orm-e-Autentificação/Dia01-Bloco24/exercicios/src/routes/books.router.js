26c242a6e1103938a6a492b0621e0dcb104d2da1
Samuel <samuelsoutosilva14@gmail.com>
2022-07-07 19:41:35 -0300

    feat: add routes to books

---
+++ Mod03-BackEnd/Bloco24-NodeJs:Orm-e-Autentificação/Dia01-Bloco24/exercicios/src/routes/books.router.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const booksController = require('../controllers/book.controller');
+
+router.get('/', booksController.getAllBooks);
+
+router.get('/:id', booksController.getByIdBook);
+
+router.post('/', booksController.create);
+
+router.put('/:id', booksController.update);
+
+router.delete('/:id', booksController.destroy);
+
+module.exports = router;
