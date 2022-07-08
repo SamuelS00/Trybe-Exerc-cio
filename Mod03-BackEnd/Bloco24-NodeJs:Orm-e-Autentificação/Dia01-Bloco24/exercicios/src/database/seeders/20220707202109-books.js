8a82072891ad53a81c3b5f94d83fbfb014cc0013
Samuel <samuelsoutosilva14@gmail.com>
2022-07-08 15:05:21 -0300

    feat: add new values ​​for publisher column

--- Mod03-BackEnd/Bloco24-NodeJs:Orm-e-Autentificação/Dia01-Bloco24/exercicios/src/database/seeders/20220707202109-books.js
+++ Mod03-BackEnd/Bloco24-NodeJs:Orm-e-Autentificação/Dia01-Bloco24/exercicios/src/database/seeders/20220707202109-books.js
@@ -5,10 +5,11 @@ module.exports = {
     [
       { 
         title: 'As Crônicas de Nárnia',
-        author: 'C. S Lewis',
+        author: 'C. S. Lewis',
         page_quantity: '345',
         created_at: Sequelize.literal('CURRENT_TIMESTAMP'),
         updated_at: Sequelize.literal('CURRENT_TIMESTAMP'),
+        publisher: 'Martins Fontes'
       },
       { 
         title: 'Eu, Robô',
@@ -16,7 +17,16 @@ module.exports = {
         page_quantity: '253',
         created_at: Sequelize.literal('CURRENT_TIMESTAMP'),
         updated_at: Sequelize.literal('CURRENT_TIMESTAMP'),
-      }
+        publisher: 'Pocket Ouro'
+      },
+      { 
+        title: 'Cristianismo Puro e simples',
+        author: 'C. S. Lewis',
+        page_quantity: '249',
+        created_at: Sequelize.literal('CURRENT_TIMESTAMP'),
+        updated_at: Sequelize.literal('CURRENT_TIMESTAMP'),
+        publisher: 'Thomas Nelson Brasil'
+      },
     ]
   ),
 
