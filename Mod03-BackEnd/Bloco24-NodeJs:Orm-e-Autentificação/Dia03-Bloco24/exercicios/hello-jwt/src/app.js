4ab7aff80d6bcfcebcb9e9e34fe85c537d9428df
Samuel <samuelsoutosilva14@gmail.com>
2022-07-13 16:34:59 -0300

    feat: add valid token

--- Mod03-BackEnd/Bloco24-NodeJs:Orm-e-Autentificação/Dia03-Bloco24/exercicios/hello-jwt/src/app.js
+++ Mod03-BackEnd/Bloco24-NodeJs:Orm-e-Autentificação/Dia03-Bloco24/exercicios/hello-jwt/src/app.js
@@ -4,12 +4,14 @@ const bodyParser = require('body-parser');
 const app = express();
 
 const ErrorMiddleware = require('./middleware/errorMiddleware');
+const authRouter = require('./routes/auth.router');
 const userRouter = require('./routes/user.router');
 
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 
+app.use('/signin', authRouter);
 app.use('/users', userRouter);
 
 app.use(ErrorMiddleware);
--- Mod03-BackEnd/Bloco24-NodeJs:Orm-e-Autentificação/Dia03-Bloco24/exercicios/hello-jwt/src/routes/user.router.js
+++ Mod03-BackEnd/Bloco24-NodeJs:Orm-e-Autentificação/Dia03-Bloco24/exercicios/hello-jwt/src/routes/user.router.js
@@ -2,8 +2,9 @@ const express = require('express');
 const rescue = require('express-rescue');
 const userRouter = express.Router();
 
+const validateToken = require('../auth/validateJWT');
 const userController = require('../controllers/user.controller');
 
-userRouter.get('/', rescue(userController.getAll));
+userRouter.get('/', validateToken, rescue(userController.getAll));
 
 module.exports = userRouter;
