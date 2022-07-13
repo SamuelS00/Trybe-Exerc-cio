75d1d0ca95d5ecf6d077406a949fc7a878ca28cd
Samuel <samuelsoutosilva14@gmail.com>
2022-07-13 17:39:22 -0300

    fix: route /users/me

--- Mod03-BackEnd/Bloco24-NodeJs:Orm-e-Autentificação/Dia03-Bloco24/exercicios/hello-jwt/src/routes/user.router.js
+++ Mod03-BackEnd/Bloco24-NodeJs:Orm-e-Autentificação/Dia03-Bloco24/exercicios/hello-jwt/src/routes/user.router.js
@@ -2,9 +2,9 @@ const express = require('express');
 const rescue = require('express-rescue');
 const userRouter = express.Router();
 
-const validateToken = require('../auth/validateJWT');
+const { validateToken } = require('../auth/validateJWT');
 const userController = require('../controllers/user.controller');
 
-userRouter.get('/', validateToken, rescue(userController.getAll));
+userRouter.get('/me', validateToken, rescue(userController.getAll));
 
 module.exports = userRouter;
