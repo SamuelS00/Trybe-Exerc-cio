832f1fdeb2ac353899a0a3420ea180d0856e3266
Samuel <samuelsoutosilva14@gmail.com>
2022-07-30 23:17:00 -0300

    feat(auth/service): add class jwtoken incomplete

---
+++ Mod03-BackEnd/Bloco26-TypeScript/Dia03-Bloco26/exercicios/src/services/jwt.service.ts
@@ -0,0 +1,19 @@
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const { JWT_SECRET } = process.env;
+
+class JwtToken {
+  
+  constructor() {
+
+  };
+
+  public createToken():  
+
+
+};
+
+export default JwtToken;
