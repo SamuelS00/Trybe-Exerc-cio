48cd6a26392a4aa6d0ff392b37cb700abf842c0b
Samuel <samuelsoutosilva14@gmail.com>
2022-08-04 21:09:54 -0300

    feat: add exercicios 4

---
+++ Mod03-BackEnd/Bloco27-POO-e-Solid/Dia02-Bloco27/exercicios/Subject.ts
@@ -0,0 +1,21 @@
+class Subject {
+  constructor(private name: string) { };
+
+  private validateName(name: string): string | Error {
+    if (!(name.length > 3)) {
+        throw new Error('the subject name must be longer than 3 characters')
+    };
+    
+    return name;
+  };
+
+  get getName(): string  | void {
+    if(this.validateName(this.name)) {
+      return this.name
+    }
+  };
+};
+
+const mate = new Subject('Matemática');
+const port = new Subject('Português');
+const geo = new Subject('Geografia');
