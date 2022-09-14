d289fc37b71267d729c3ea678031baf99c4b9f35
Samuel <samuelsoutosilva14@gmail.com>
2022-09-14 13:16:33 -0300

    fix: add custom error in launcher

--- Mod03-BackEnd/Bloco30-Mongoose-Arquitetura-MSC/Dia01-Bloco30/src/models/MongoModel.ts
+++ Mod03-BackEnd/Bloco30-Mongoose-Arquitetura-MSC/Dia01-Bloco30/src/models/MongoModel.ts
@@ -1,4 +1,5 @@
 import { isValidObjectId, Model } from 'mongoose';
+import { ErrorTypes } from '../errors/catalog';
 import IModel from '../interfaces/IModel';
 
 // o do solid, aberta para extensão, fechada para modificação. 
@@ -20,9 +21,11 @@ abstract class MongoModel<T> implements IModel<T> { // implementa um contrato a 
   }
 
   public async readOne(_id:string):Promise<T | null> {
-    if (!isValidObjectId(_id)) throw Error('InvalidMongoId');
+    if (!isValidObjectId(_id)) throw new Error(ErrorTypes.InvalidMongoId);
+
     // isValidObjectId: comumente obervamos que o db retorna um id automatico.
     // moongose testa se esse objeto é válido.
+    
     return this._model.findOne({ _id });
   }
 
