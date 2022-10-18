4d7698854d1be7b5d12cef92c4813fc7fc1f5b7e
Samuel <samuelsoutosilva14@gmail.com>
2022-10-18 11:41:16 -0300

    refactor: print_square function

--- Mod04-CienciaDaComputacao/Bloco31-Introducao-a-Python/Dia01-Bloco31/exercicios/print_square.py
+++ Mod04-CienciaDaComputacao/Bloco31-Introducao-a-Python/Dia01-Bloco31/exercicios/print_square.py
@@ -1,11 +1,6 @@
 def print_square(n: int):
-    column, row = 0, 0
-    while column < n:
-        column += 1
-        while row < n:
-            row += 1
-            asterisk = '*' * n
-            print(asterisk)
+    for row in range(n):
+        print(n * '*')
 
 
 print_square(5)
