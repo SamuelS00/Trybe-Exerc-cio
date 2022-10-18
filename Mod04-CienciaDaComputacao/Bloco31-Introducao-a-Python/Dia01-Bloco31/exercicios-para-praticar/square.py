acab152be7616a9b370512c92c407aced065a184
Samuel <samuelsoutosilva14@gmail.com>
2022-10-18 11:56:11 -0300

    feat: add square example

--- Mod04-CienciaDaComputacao/Bloco31-Introducao-a-Python/Dia01-Bloco31/exercicios-para-praticar/square.py
+++ Mod04-CienciaDaComputacao/Bloco31-Introducao-a-Python/Dia01-Bloco31/exercicios-para-praticar/square.py
@@ -1,10 +1,4 @@
 # retornar quadrados de 1 a 10;
 
-quadrados = [x*x for x in range(11)] # utliza comprehension list. range(11) cria uma lista de 11 posições. e para cada x, retorne x * x. ex 2 * 2 igual 4.
-
+quadrados = [x*x for x in range(11)]
 print(quadrados)
-
-# Saída
-[0, 1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
-
-
