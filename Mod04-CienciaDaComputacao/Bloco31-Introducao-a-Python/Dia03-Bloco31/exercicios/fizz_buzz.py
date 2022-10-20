89168ed938be74e3ec5ed0bb0267dd10ec509adf
Samuel <samuelsoutosilva14@gmail.com>
2022-10-20 12:18:21 -0300

    refactor: add fizz buzz list return

--- Mod04-CienciaDaComputacao/Bloco31-Introducao-a-Python/Dia03-Bloco31/exercicios/fizz_buzz.py
+++ Mod04-CienciaDaComputacao/Bloco31-Introducao-a-Python/Dia03-Bloco31/exercicios/fizz_buzz.py
@@ -4,7 +4,7 @@ def fizz_buzz(n: int):
 
     for num in range(1, n + 1):
         if num % 3 == 0 and num % 5 == 0:
-            fizz_buzz_list.append('Fizzbuzz')
+            fizz_buzz_list.append('FizzBuzz')
         elif num % 3 == 0:
             fizz_buzz_list.append('Fizz')
         elif num % 5 == 0:
@@ -12,7 +12,8 @@ def fizz_buzz(n: int):
         else:
             fizz_buzz_list.append(num)
 
-    print(fizz_buzz_list)
+    return fizz_buzz_list
 
 
-fizz_buzz(10)
+# fizz_buzz(10)
+print(fizz_buzz(3)[-1])
