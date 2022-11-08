d6a10336bfcd5a05a40cb9e7b9232d8c97f97e41
Samuel <samuelsoutosilva14@gmail.com>
2022-11-08 17:30:43 -0300

    feat: add bsoup_example

--- Mod04-CienciaDaComputacao/Bloco34-Redes-e-Raspagem-de-dados/Dia03-Bloco34/exercicios-praticar/bsoup_example.py
+++ Mod04-CienciaDaComputacao/Bloco34-Redes-e-Raspagem-de-dados/Dia03-Bloco34/exercicios-praticar/bsoup_example.py
@@ -1,5 +1,10 @@
 import requests
+from bs4 import BeautifulSoup
 
 url = "https://quotes.toscrape.com"
 page = requests.get(url)
-print(page.content)
+html_content = page.text
+
+soup = BeautifulSoup(html_content, "html.parser")
+
+print(soup.prettify())
