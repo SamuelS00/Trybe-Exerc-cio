af0440e6e6d2a6af3c6b360b2e1a94b90c7a002c
Samuel <samuelsoutosilva14@gmail.com>
2022-12-03 14:11:37 -0300

    feat: add stack example

---
+++ Mod04-CienciaDaComputacao/Bloco36-Estrutura-de-Dados/Dia04-Bloco36/examples/stack.py
@@ -0,0 +1,69 @@
+class Stack:
+    def __init__(self):
+        self._data = list()
+
+    def size(self):
+        return len(self._data)
+
+    def is_empty(self):
+        return not bool(self.size())
+
+    def push(self, value):
+        self._data.append(value)
+
+    def pop(self):
+        if self.is_empty():
+            return None
+
+        # -1 se refere ao último objeto da pilha.
+        # Ou seja, o valor do topo da pilha
+        value = self._data[-1]
+        del self._data[-1]
+        return value
+
+    def peek(self):
+        if self.is_empty():
+            return None
+        value = self._data[-1]
+        return value
+
+    def clear(self):
+        self._data.clear()
+
+    def __str__(self):
+        str_items = ""
+        for i in range(self.size()):
+            value = self._data[i]
+            str_items += str(value)
+            if i + 1 < self.size():
+                str_items += ", "
+
+        return "Stack(" + str_items + ")"
+
+
+if __name__ == "__main__":
+    elements = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    content_stack = Stack()
+
+    for elem in elements:
+        content_stack.push(elem)
+
+    # saída: Stack(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    print(content_stack)
+    # saída: 10
+    print(content_stack.size())
+
+    # saída: 10
+    print(content_stack.peek())
+    # saída: 10, pois a função retorna o elemento que está sendo retirado
+    print(content_stack.pop())
+
+    # saída: 9, pois, após o 10 ter sido removido, o 9 se tornou o elemento do topo da pilha
+    print(content_stack.peek())
+    # saída: 9
+    print(content_stack.size())
+
+    # saída: None, pois a função não retorna nada!/learn/course/5e938f69-6e32-43b3-9685-c936530fd326/module/290e715d-73e3-4b2d-a3c7-4fe113474070/section/7939c08a-6df3-4e20-81f3-9581c68d940b/day/c55250a2-6d5d-4dba-a336-4e9a9a0cc6b5/lesson/d0a17243-9f22-4501-86cb-d96030e7d73b
+    print(content_stack.clear())
+    # saída: 0
+    print(content_stack.size())
