a091c3904f86772e364aa3e8e87b5d2ecdfe5821
Samuel <samuelsoutosilva14@gmail.com>
2022-12-12 16:47:18 -0300

    feat: add method add in class set

---
+++ Mod04-CienciaDaComputacao/Bloco37-Estrutura-de-Dados-II/Dia02-Bloco37/exercicios-praticar/set_add.py
@@ -0,0 +1,15 @@
+from set import Set
+
+
+def main():
+    # exercise 1
+    set_add_print = Set()
+    for num in [0, 10, 100, 1000]:
+        set_add_print.add(num)
+
+    # exercise 2
+    print(set_add_print)
+
+
+if __name__ == "__main__":
+    main()
